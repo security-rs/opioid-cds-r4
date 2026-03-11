203dc995fa9ac8d0365014435a1e0a6aebb0de52
Mohammad Jafari <jafarim@gmail.com>
2026-03-10 17:51:35 -0700

    CI test coverage (#389)
    * update deps
    * add all pre-fetch tests
    * restructure and add without-pre-fetch tests
    * add basic card cardinality assertions
    * disable assertions for some tests with known issues.

--- tests/test/helpers/date-helpers.js
+++ tests/test/helpers/date-helpers.js
@@ -1,8 +1,8 @@
-const now = new Date();
+const now = () => new Date();
 
 const toISOStringNoTz = (date) => date.toISOString().replace("Z", "");
 
-const currentDateTimeNoTz = () => toISOStringNoTz(now);
+const currentDateTimeNoTz = () => toISOStringNoTz(now());
 
 const addDays = (date, days) =>
   new Date(new Date(date).setDate(date.getDate() + days));
@@ -10,20 +10,34 @@ const addDays = (date, days) =>
 const subtractDays = (date, days) =>
   new Date(new Date(date).setDate(date.getDate() - days));
 
-const getDateTimeMinusDays = (days) => toISOStringNoTz(subtractDays(now, days));
+const getDateTimeMinusDays = (days) =>
+  toISOStringNoTz(subtractDays(now(), days));
 
-const getDateTimePlusDays = (days) => toISOStringNoTz(addDays(now, days));
+const getDateTimePlusDays = (days) => toISOStringNoTz(addDays(now(), days));
 
-const todayDateTime = currentDateTimeNoTz();
-const dateTimeMinus30Days = getDateTimeMinusDays(30);
-const dateTimePlus30Days = getDateTimePlusDays(30);
-const yesterdayDateTime = getDateTimeMinusDays(1);
-const nowNoTz = currentDateTimeNoTz();
+const todayDateTime = () => currentDateTimeNoTz();
+const dateTimeMinus30Days = () => getDateTimeMinusDays(30);
+const dateTimePlus30Days = () => getDateTimePlusDays(30);
+const yesterdayDateTime = () => getDateTimeMinusDays(1);
+const nowNoTz = () => currentDateTimeNoTz();
+
+function getDateMinusYears(years) {
+  const d = new Date();
+  d.setFullYear(d.getFullYear() - years);
+  // Format yyyy-mm-dd
+  const yyyy = d.getFullYear();
+  const mm = String(d.getMonth() + 1).padStart(2, "0");
+  const dd = String(d.getDate()).padStart(2, "0");
+  return `${yyyy}-${mm}-${dd}`;
+}
 
 module.exports = {
   nowNoTz,
   todayDateTime,
+  getDateTimeMinusDays,
+  getDateTimePlusDays,
   dateTimeMinus30Days,
   dateTimePlus30Days,
-  yesterdayDateTime
+  yesterdayDateTime,
+  getDateMinusYears
 };
